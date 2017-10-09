4d66ccc51f7e1626cb96c9ef042aade930ef5495
Jean-Marie Kubek <jkubek@univ-jfc.fr>
2017-10-09 14:52:31 +0200

    More tests

---
+++ oncrpc4j-core/src/test/java/org/dcache/libvirt/ClientServerIntegrationWithAuthlist.java
@@ -0,0 +1,158 @@
+package org.dcache.libvirt;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import org.dcache.xdr.IpProtocolType;
+import org.dcache.xdr.OncRpcException;
+import org.dcache.xdr.OncRpcProgram;
+import org.dcache.xdr.OncRpcSvc;
+import org.dcache.xdr.RpcCall;
+import org.dcache.xdr.RpcDispatchable;
+import org.dcache.xdr.XdrAble;
+import org.dcache.xdr.XdrDecodingStream;
+import org.dcache.xdr.XdrEncodingStream;
+import org.dcache.xdr.XdrInt;
+import org.dcache.xdr.XdrString;
+import org.dcache.xdr.XdrTransport;
+import org.dcache.xdr.XdrVoid;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.libvirt.VirOncRpcSvcBuilder;
+import org.libvirt.VirRpcCall;
+
+import static org.junit.Assert.*;
+
+/**
+ * Interop tests between a virtrpc client and a server 
+ * 
+ */
+public class ClientServerIntegrationWithAuthlist {
+
+    private static final int PROGNUM = 536903814;
+    private static final int PROGVER = 1;
+
+    private OncRpcSvc svc;
+    private OncRpcSvc clnt;
+    private VirRpcCall clntCall;
+
+    @Before
+    public void setUp() throws IOException {
+
+        RpcDispatchable fakeLibvirtd = (RpcCall aCall) -> {
+            assertTrue(aCall instanceof VirRpcCall);
+            VirRpcCall call = (VirRpcCall) aCall;
+            call.getXdr().asBuffer().mark();
+            call.getXdr().asBuffer().reset();
+            
+            
+            
+            switch (call.getProcedure()) {
+
+                case 1: {
+                    XdrString s = new XdrString();
+
+                    XdrAble connectArgs = new  XdrAble() {
+
+                        String name;
+                        int flags;
+                        @Override
+                        public void xdrDecode(XdrDecodingStream xdr) throws OncRpcException, IOException {
+                            xdr.xdrDecodeBoolean(); 
+                            name=xdr.xdrDecodeString();
+                            flags = xdr.xdrDecodeInt();
+                            assertEquals("test:///default",name);
+                            assertEquals(0,flags);
+                            
+                        }
+
+                        @Override
+                        public void xdrEncode(XdrEncodingStream xdr) throws OncRpcException, IOException {
+                        }
+                        
+                    };
+                    call.retrieveCall(connectArgs);
+                    call.reply(s);
+                    break;
+                }
+                case 2: {
+                    svc.stop();
+                    break;
+                }
+                case 66 : { 
+                    //AUthlist : args void
+                    XdrInt res = new XdrInt(0);
+                    call.reply(res);
+                    break;
+                }
+                case 60 : { //features : args int
+                    XdrInt res = new XdrInt(0);
+                    call.reply(res);
+                    break;
+                }
+                case 59 : { 
+                    XdrString res = new XdrString("fakelibvirthost");
+                    call.reply(res);
+                    break;
+                }
+                
+                default :
+                    fail("Unknown proc " + call.getProcedure());
+            }
+        };
+
+        svc = new VirOncRpcSvcBuilder()
+                .withTCP()
+                .withPort(20000)
+                .withWorkerThreadIoStrategy()
+                .withRpcService(new OncRpcProgram(PROGNUM, PROGVER), fakeLibvirtd)
+                .build();
+        svc.start();
+        
+       
+        clnt = new VirOncRpcSvcBuilder()
+                .withTCP()
+                .withClientMode()
+                .withWorkerThreadIoStrategy()
+                .build();
+        clnt.start();
+        XdrTransport t = clnt.connect(svc.getInetSocketAddress(IpProtocolType.TCP));
+        clntCall = new VirRpcCall(PROGNUM, PROGVER, null, t);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        if (svc != null) {
+            svc.stop();
+        }
+        if (clnt != null) {
+            clnt.stop();
+        }
+    }
+    @Test
+    public void callVirshAuthList() throws IOException{
+        XdrInt reply = new XdrInt();
+        clntCall.call(66, XdrVoid.XDR_VOID, reply);
+        assertEquals(0,reply.intValue());
+        XdrAble connectArgs = new  XdrAble() {
+            @Override
+            public void xdrDecode(XdrDecodingStream xdr) throws OncRpcException, IOException {
+                
+            }
+
+            @Override
+            public void xdrEncode(XdrEncodingStream xdr) throws OncRpcException, IOException {
+                xdr.xdrEncodeBoolean(true);
+                xdr.xdrEncodeString("test:///default");
+                xdr.xdrEncodeInt(0);
+                
+            }
+            
+        };
+        clntCall.call(1,connectArgs ,  XdrVoid.XDR_VOID);
+        
+    }
+    
+
+}
