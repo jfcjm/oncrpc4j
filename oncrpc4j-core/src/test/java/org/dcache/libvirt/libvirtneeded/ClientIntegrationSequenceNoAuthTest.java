295cb5e82be5aafcc269b99da9eb090645e56f28
Jean-Marie Kubek <jkubek@univ-jfc.fr>
2017-10-09 14:53:30 +0200

    rename package for tests using virsh/libvirt

--- oncrpc4j-core/src/test/java/org/dcache/libvirt/libvirtneeded/ClientIntegrationSequenceNoAuthTest.java
+++ oncrpc4j-core/src/test/java/org/dcache/libvirt/libvirtneeded/ClientIntegrationSequenceNoAuthTest.java
@@ -1,4 +1,4 @@
-package org.dcache.libvirt.libvirtnedded;
+package org.dcache.libvirt.libvirtneeded;
 
 import static org.junit.Assert.*;
 
--- oncrpc4j-core/src/test/java/org/dcache/libvirt/libvirtneeded/ServerIntegration.java
+++ oncrpc4j-core/src/test/java/org/dcache/libvirt/libvirtneeded/ServerIntegration.java
@@ -1,4 +1,4 @@
-package org.dcache.libvirt.libvirtnedded;
+package org.dcache.libvirt.libvirtneeded;
 
 import java.io.DataInputStream;
 import java.io.IOException;
